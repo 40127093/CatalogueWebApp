0908c7e260d8d26b98b95f88cd1b6ce8f15371bd
40120793 <40127093@live.napier.ac.uk>
2016-10-22 09:02:53 +0000

    Created the config file and modified the initialisation app code accordingly

--- src/index.py
+++ src/index.py
@@ -1,10 +1,27 @@
+import ConfigParser
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-  return render_template('layout.html'), 200
+  return render_template('home.html'), 200
 
+def init (app):
+  config = ConfigParser.ConfigParser()
+  try:
+    config_location = "etc/defaults.cfg"
+    config.read(config_location)
+
+    app.config['DEBUG'] = config.get("config", "debug")
+    app.config['ip_address'] = config.get("config", "ip_address")
+    app.config['port'] = config.get("config", "port")
+    app.config['url'] = config.get("config", "url")
+  except:
+    print "Could not read configuration file from: " , config_location
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True)
+  init(app)
+  app.run(
+    host = app.config['ip_address'],
+    port = int(app.config['port']))
